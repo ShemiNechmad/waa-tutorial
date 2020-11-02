ec46fb62e53605cf4e1a8416166f3bca13b3baad
Shemi Nechmad <crunchies@mail.com>
2020-11-02 02:18:21 +0200

    chapter 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'waa-tutorial';
+export class AppComponent implements OnInit {
+
+  private ctx: AudioContext;
+
+  constructor() { }
+
+  ngOnInit() {
+    AudioContext = (window as any).AudioContext || (window as any).webkitAudioContext;
+    this.ctx = new AudioContext();
+  }
+
+  play() {
+    let osc = this.ctx.createOscillator();
+    osc.onended = () => osc.disconnect();
+    osc.connect(this.ctx.destination);
+    osc.frequency.value = 523.3;
+    osc.start();
+    osc.stop(this.ctx.currentTime + 1.5);
+  }
 }
